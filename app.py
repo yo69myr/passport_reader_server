4d96b4aaea9216912bee8c3e362e7011894ecdcf
yo69myr <554499@ukr.net>
2025-06-14 19:14:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def auth():
     return jsonify({"status": "error", "message": "Невірний логін або пароль"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, ssl_context="adhoc")
+    app.run(host="0.0.0.0", port=5000)
